869a31cb509866fcc8f8e7749738ca11d80d75f5
Oscar Recio <reciog.oscar@hotmail.com>
2026-04-01 04:16:38 -0600

    feat: add auth pages

---
+++ app/pages/confirm.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'simple'
+})
+// const user = useSupabaseUser()
+
+// watch(user, () => {
+//   if (user.value) {
+//       // Redirect to protected page
+//       return navigateTo('/')
+//   }
+// }, { immediate: true })
+</script>
+
+<template>
+  <div class="flex justify-center"><UIcon name="i-lucide-boxes" class="size-5 animate-spin" /></div>
+</template>
